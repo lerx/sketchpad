c035fb75fc2e5ffa7dc74a7b9ea93e9ef89bd7b3
lerx <scottw213@gmail.com>
2014-10-27 21:43:49 -0400

    Added hover effect

--- js/sketchpad.js
+++ js/sketchpad.js
@@ -1,12 +1,15 @@
-console.log("Hello world!");
-
 $(document).ready(function() {
-	for (var i = 0; i < 16; i++) {
-		for (var j = 0; j < 16; j++) {
+	for (var i = 0; i < 32; i++) {
+		for (var j = 0; j < 32; j++) {
 			$(".container").append("<div class='square'></div>");
 		}
-		//$(".container").append("<br/>");
+		$(".container").append("<div class='new_row'></div>");
 	}
-	$(".square").css("width", ($(".container").width()/16) - 2);
-	$(".square").css("height", ($(".container").width()/16) - 2);
+	var square_size = ($(".container").width() / 32) - 2.5;
+	$(".square").css("width", square_size);
+	$(".square").css("height", square_size);
+
+	$(document).on("mouseenter", ".square", function() {
+		$(this).addClass("square_highlight");
+	});
 });
